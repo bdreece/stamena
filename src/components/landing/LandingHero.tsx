2ad9fb891ab5d27ccb9000e5855c8e5f5af02e5b
Brian Reece <bdreece@mtu.edu>
2022-07-17 17:00:51 -0400

    Updated URL in landing hero

--- src/components/landing/LandingHero.tsx
+++ src/components/landing/LandingHero.tsx
@@ -1,12 +1,12 @@
+import { signIn } from 'next-auth/react';
 import Image from 'next/image';
-import Link from 'next/link';
 
 const LandingHero = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <Image
-          src="https://fjrflrzxudmtlaaltbjc.supabase.co/storage/v1/object/public/photos/LILYGO-T-Higrow-ESP32-Soil-Tester-DHT11-BEM280-Garden-Flowers-Temperature-Moisture-Sensor-WiFi-Bluetooth-Wireless.jpg_Q90.jpg_.webp"
+          src="https://storage.googleapis.com/stamena.bdreece.dev/LILYGO-T-Higrow-ESP32-Soil-Tester-DHT11-BEM280-Garden-Flowers-Temperature-Moisture-Sensor-WiFi-Bluetooth-Wireless.jpg_Q90.jpg_.webp"
           className="max-w-sm rounded-lg shadow-xl"
           width={336}
           height={336}
@@ -23,9 +23,9 @@ const LandingHero = () => {
             best plant parent, and seeing where you stack up on the global plant
             care leaderboards.
           </p>
-          <Link href="/api/auth/login">
-            <div className="btn btn-primary">Get Started</div>
-          </Link>
+          <button className="btn btn-primary" onClick={() => signIn()}>
+            Get Started
+          </button>
         </div>
       </div>
     </div>

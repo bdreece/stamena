1e75f41873564d2594b64b5939412af1ccc25a00
Brian Reece <bdreece@mtu.edu>
2022-07-16 23:10:33 -0400

    Added conditional rendering based on user status

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -5,31 +5,49 @@ import Drawer from 'components/Drawer';
 import SearchBar from 'components/SearchBar';
 import Link from 'next/link';
 import Footer from 'components/Footer';
+import ProfileDropdown from 'components/ProfileDropdown';
 
 const Layout: React.FC<{ children: JSX.Element }> = ({ children }) => {
   const { user } = useUser();
-  let items = [
-    <SearchBar searchFunc={() => {}} key={0} />,
-    <Link href="/leaderboard" key={1}>
-      <div className="btn btn-primary rounded-lg display">Leaderboard</div>
-    </Link>,
-    <Link href={user ? '/api/auth/login' : '/api/auth/logout'} key={2}>
-      <div className="btn btn-success text-light rounded-lg display">
-        {user ? 'Log In' : 'Log Out'}
-      </div>
-    </Link>,
-  ];
 
   return (
     <div className="drawer">
       <input id="my-drawer" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col">
-        <Navbar>{items}</Navbar>
+        <Navbar>
+          <SearchBar searchFunc={() => {}} key={0} />
+          <Link href="/leaderboard" key={1}>
+            <div className="btn btn-primary rounded-lg display">
+              Leaderboard
+            </div>
+          </Link>
+          {user ? (
+            <ProfileDropdown user={user} />
+          ) : (
+            <Link href="/api/auth/login">
+              <div className="btn btn-success text-light rounded-lg display">
+                Log In
+              </div>
+            </Link>
+          )}
+        </Navbar>
         <div className="flex-1">{children}</div>
         <Footer />
       </div>
       <div className="drawer-side">
-        <Drawer>{items}</Drawer>
+        <Drawer>
+          <SearchBar searchFunc={() => {}} key={0} />
+          <Link href="/leaderboard" key={1}>
+            <div className="btn btn-primary rounded-lg display">
+              Leaderboard
+            </div>
+          </Link>
+          <Link href={!user ? '/api/auth/login' : '/api/auth/logout'} key={2}>
+            <div className="btn btn-success text-light rounded-lg display">
+              {!user ? 'Log In' : 'Log Out'}
+            </div>
+          </Link>
+        </Drawer>
       </div>
     </div>
   );

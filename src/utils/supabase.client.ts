39b3d6e0d4c6b6869a101cd48bbac47ab4ecba57
Brian Reece <bdreece@mtu.edu>
2022-07-17 00:18:01 -0400

    Renamed supabase.ts

--- src/utils/supabase.client.ts
+++ src/utils/supabase.client.ts
@@ -1,4 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
+import { UserProfile } from '@auth0/nextjs-auth0';
+import { createClient, User } from '@supabase/supabase-js';
 
 const NEXT_PUBLIC_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL ?? '';
 const NEXT_PUBLIC_SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY ?? '';

f04854c19b15f4b29c160572256f3dfb0b440014
Brian Reece <bdreece@mtu.edu>
2022-07-17 00:17:50 -0400

    Added landing refactor to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next';
+
 import Head from 'next/head';
-import Image from 'next/image';
+
+import { useUser } from '@auth0/nextjs-auth0';
+
+import LandingHero from 'components/landing/LandingHero';
+import LandingFeed from 'components/landing/LandingFeed';
 
 const Home: NextPage = () => {
+  const { user } = useUser();
   return (
     <>
       <Head>
@@ -10,30 +16,7 @@ const Home: NextPage = () => {
         <meta name="description" content="Finally, competitive plant care" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <Image
-            src="https://fjrflrzxudmtlaaltbjc.supabase.co/storage/v1/object/public/photos/LILYGO-T-Higrow-ESP32-Soil-Tester-DHT11-BEM280-Garden-Flowers-Temperature-Moisture-Sensor-WiFi-Bluetooth-Wireless.jpg_Q90.jpg_.webp"
-            className="max-w-sm rounded-lg shadow-xl"
-            width={336}
-            height={336}
-            alt="LilyGo T-Grow"
-          />
-          <div>
-            <h1 className="text-5xl font-bold display">
-              Finally, competitive plant care
-            </h1>
-            <p className="py-6 max-w-lg">
-              With Stamena, you can connect your LILYGO TT-Higrow plant monitor
-              to your profile to start tracking your plants&apos; hydration and
-              environment quality, competing with friends to see who can be the
-              best plant parent, and seeing where you stack up on the global
-              plant care leaderboards.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div>
+      {user ? <LandingFeed user={user} /> : <LandingHero />}
     </>
   );
 };

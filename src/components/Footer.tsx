c3bc341011a3572222223c0f8f3ca87a087717aa
Brian Reece <bdreece@mtu.edu>
2022-07-16 23:10:20 -0400

    Rounded footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content rounded-lg">
       <div>
         <p>Copyright © 2022 Brian Reece</p>
       </div>

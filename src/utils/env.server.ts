0398a1af1f684f2702a90f9568dba0cbc0e27d8b
Brian Reece <bdreece@mtu.edu>
2022-07-17 14:52:16 -0400

    Added server env module

--- src/utils/env.server.ts
+++ src/utils/env.server.ts
@@ -1,2 +1,6 @@
 export const SUPABASE_SIGNING_SECRET =
   process.env.SUPABASE_SIGNING_SECRET ?? '';
+
+export const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID ?? '';
+export const AUTH0_CLIENT_SECRET = process.env.AUTH0_CLIENT_SECRET ?? '';
+export const AUTH0_ISSUER_BASE_URL = process.env.AUTH0_ISSUER_BASE_URL ?? '';

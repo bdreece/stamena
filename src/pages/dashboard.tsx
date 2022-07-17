d15857c47c6a7d0d91006cd2cf1e3551f2bd4441
Brian Reece <bdreece@mtu.edu>
2022-07-17 00:17:34 -0400

    Added auth to dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,13 @@
-import type { NextPage } from 'next';
+import { UserProfile, withPageAuthRequired } from '@auth0/nextjs-auth0';
+import type { GetServerSideProps, NextPage } from 'next';
+import { useState } from 'react';
+import { getSupabase } from 'utils/supabase.client';
 
-const Dashboard: NextPage = () => {
-  return <></>;
+export const getServerSideProps: GetServerSideProps<UserProfile> =
+  withPageAuthRequired();
+
+const Dashboard: NextPage<{ user: UserProfile }> = ({ user }) => {
+  return <div className="flex flex-wrap"></div>;
 };
 
 export default Dashboard;

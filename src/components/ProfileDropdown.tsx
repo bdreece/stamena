cb51692e2f3ae626acbfb0e00674515655544d2c
Brian Reece <bdreece@mtu.edu>
2022-07-16 23:27:35 -0400

    Implemented more of profile dropdown

--- src/components/ProfileDropdown.tsx
+++ src/components/ProfileDropdown.tsx
@@ -1,18 +1,34 @@
 import type { UserProfile } from '@auth0/nextjs-auth0';
+import Link from 'next/link';
 import Image from 'next/image';
 
-const ProfileDropdown: React.FC<{ user: UserProfile }> = ({ user }) => {
+interface ProfileDropdownProps {
+  user: UserProfile;
+  end?: boolean;
+  label?: string;
+}
+
+const ProfileDropdown: React.FC<ProfileDropdownProps> = ({
+  user,
+  end,
+  label,
+}) => {
   return (
-    <div className="dropdown dropdown-end">
+    <div className={end ? 'dropdown dropdown-end' : 'dropdown'}>
       <label tabIndex={0}>
         {user.picture ? (
-          <div className="avatar">
-            <div className="mask mask-squircle">
-              <Image src={user.picture} height={32} width={32} alt="Menu" />
+          <div className="flex justify-around">
+            <div className="avatar">
+              <div className="mask mask-squircle">
+                <Image src={user.picture} height={32} width={32} alt="Menu" />
+              </div>
             </div>
+            {label ? <p className="ml-2 display">{label}</p> : <div />}
           </div>
         ) : (
-          <button className="btn btn-primary display">Profile</button>
+          <button className="btn btn-primary display">
+            {label ? label : 'Profile'}
+          </button>
         )}
       </label>
       <ul
@@ -20,10 +36,19 @@ const ProfileDropdown: React.FC<{ user: UserProfile }> = ({ user }) => {
         className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 mt-48"
       >
         <li>
-          <a>Item 1</a>
+          <Link className="display" href="/profile">
+            Profile
+          </Link>
         </li>
         <li>
-          <a>Item 2</a>
+          <Link className="display" href="/dashboard">
+            Dashboard
+          </Link>
+        </li>
+        <li>
+          <Link className="display" href="/api/auth/logout">
+            Log Out
+          </Link>
         </li>
       </ul>
     </div>
